f338b0e992161b01ec1cdada0183d25389b3d331
Kari - Zenith <agustin@orbita2.com>
2026-03-20 16:37:29 -0300

    feat(lib): add main index.ts for barrel exports
    - Export all constants, utils, hooks, and exports from single entry point
    - Enables clean imports: import { formatAlumnoNombre, usePagination } from '@/lib'
    - Centralized access to all library utilities

---
+++ src/lib/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Biblioteca de utilidades compartidas
+ *
+ * Este módulo exporta todas las utilidades, constantes, hooks y servicios
+ * reutilizables del sistema.
+ *
+ * Uso recomendado:
+ * ```tsx
+ * import { formatAlumnoNombre, calcularDiasRestantes, usePagination } from '@/lib'
+ * ```
+ */
+
+// Constants
+export * from './constants'
+
+// Utils
+export * from './utils'
+
+// Hooks
+export * from './hooks'
+
+// Exports
+export * from './exports'
